1377ae4671fa8637ce54658ac544848bc77d02ea
Parichay Pothepalli <pxp210115@utdallas.edu>
2022-06-19 11:24:39 -0600

    Create 1667. Fix Names in a Table.sql

---
+++ 1667. Fix Names in a Table.sql
@@ -0,0 +1,43 @@
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+
+Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+Output: 
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | Alice |
+| 2       | Bob   |
++---------+-------+
+
+My Solution:-
+
+# Write your MySQL query statement below
+select user_id, 
+    concat(upper(left(name,1)),lower(substring(name,2))) as name
+from Users 
+order by user_id;
