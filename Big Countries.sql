339d0f858d40c883ea5b3290d4b5f50af0cb3521
Parichay Pothepalli <pxp210115@utdallas.edu>
2022-06-19 11:06:33 -0600

    Create Big Countries.sql

---
+++ Big Countries.sql
@@ -0,0 +1,30 @@
+-- Question 5
+-- There is a table World
+
+-- +-----------------+------------+------------+--------------+---------------+
+-- | name            | continent  | area       | population   | gdp           |
+-- +-----------------+------------+------------+--------------+---------------+
+-- | Afghanistan     | Asia       | 652230     | 25500100     | 20343000      |
+-- | Albania         | Europe     | 28748      | 2831741      | 12960000      |
+-- | Algeria         | Africa     | 2381741    | 37100000     | 188681000     |
+-- | Andorra         | Europe     | 468        | 78115        | 3712000       |
+-- | Angola          | Africa     | 1246700    | 20609294     | 100990000     |
+-- +-----------------+------------+------------+--------------+---------------+
+-- A country is big if it has an area of bigger than 3 million square km or a population of more than 25 million.
+
+-- Write a SQL solution to output big countries' name, population and area.
+
+-- For example, according to the above table, we should output:
+
+-- +--------------+-------------+--------------+
+-- | name         | population  | area         |
+-- +--------------+-------------+--------------+
+-- | Afghanistan  | 25500100    | 652230       |
+-- | Algeria      | 37100000    | 2381741      |
+-- +--------------+-------------+--------------+
+
+
+-- My Solution
+-- We use Select statement to select multiple columns needed. We then use the From statement to retrieve from the world table and use the where clause to define the criterias for filtering.
+select name, population,area from world
+where area>=3000000 or population >=25000000;
