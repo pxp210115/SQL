2d7366588ccbf2d3575dca129d4d50ad9e209129
Parichay Pothepalli <pxp210115@utdallas.edu>
2022-11-07 00:30:05 -0600

    Update 197. Rising Temperature.sql

--- Leetcode/197. Rising Temperature.sql
+++ Leetcode/197. Rising Temperature.sql
@@ -11,7 +11,7 @@ id is the primary key for this table.
 This table contains information about the temperature on a certain day.
  
 
-Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+Write an SQL query to find all dates Id with higher temperatures compared to its previous dates (yesterday).
 
 Return the result table in any order.
 
